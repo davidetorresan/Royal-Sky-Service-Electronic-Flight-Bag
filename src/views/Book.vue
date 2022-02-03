412eaf080800720c449a106c1bdd5fa5e1d60dd3
Davide Torresan <davidetorresan15@gmail.com>
2022-02-03 15:40:00 +0100

    continuing booking features

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -4,17 +4,20 @@
       <div class="flex flex-col">
         <div class="w-[100%] p-6 rounded-2xl bg-white flex flex-col justify-between items-start text-white relative">
             <ul class="divide-y-2 divide-gray-100 overflow-x-auto w-full">
-
-              <router-link v-for="(route, index) in routes.aerotaxi" :key="index" :to="'/book/'+route.route">
-                <li class="py-3 flex justify-between text-sm text-gray-500 font-semibold items-center">           
+              <li class="py-3 flex justify-between text-sm text-gray-500 font-semibold items-center">           
+                <p class="w-[5%] px-2 text-gray-600 text-center">Volo #</p>
+                <p class="w-[28%] px-2 text-gray-600 text-center">Partenza</p>
+                <p class="w-[28%] px-2 text-gray-600 text-center">Destinazione</p>
+                <p class="w-[8%] px-2 text-blue-600 text-center">Alternato</p>
+              </li> 
+              <li class="py-3 flex justify-between text-sm text-gray-500 font-semibold items-center" v-for="(route, index) in routes.aerotaxi" :key="index">           
+                <router-link :to="'/book/'+route.route" class="w-full flex justify-between">
                   <p class="w-[5%] px-2 text-gray-600 text-center">{{route.flight}}</p>
                   <p class="w-[28%] px-2 text-gray-600 text-center">{{route.departure}} - <span>{{route.dep_name}}</span></p>
                   <p class="w-[28%] px-2 text-gray-600 text-center">{{route.arrival}} - <span>{{route.arr_name}}</span></p>
                   <p class="w-[8%] px-2 text-blue-600 text-center">{{route.alternative}}</p>
-                  <p class="w-[8%] px-2 text-yellow-500 text-center">B350</p>
-                </li>
-              </router-link>
-
+                </router-link>
+              </li>
             </ul>
           <br>
         </div>
--- src/views/BookFlight.vue
+++ src/views/BookFlight.vue
@@ -7,12 +7,19 @@
         <div class="flex flex-col mt-10">
             <div class="w-[100%] p-6 rounded-2xl bg-white flex flex-col justify-between items-start text-white relative">
                 <ul class="divide-y-2 divide-gray-100 overflow-x-auto w-full text-black">
+                    <li class="py-3 flex justify-between text-sm text-gray-500 font-semibold items-center">           
+                        <p class="w-[5%] px-2 text-gray-600 text-center">Volo #</p>
+                        <p class="w-[28%] px-2 text-gray-600 text-center">Partenza</p>
+                        <p class="w-[28%] px-2 text-gray-600 text-center">Destinazione</p>
+                        <p class="w-[8%] px-2 text-blue-600 text-center">Alternato</p>
+                        <span class="cursor-pointer w-[8%] px-2 text-yellow-500 text-center"></span>
+                    </li>
                     <li class="py-3 flex justify-between text-sm text-gray-500 font-semibold items-center" v-for="(plane, index) in planes" :key="index">           
                         <p class="w-[5%] px-2 text-gray-600 text-center">{{plane.flight}}</p>
                         <p class="w-[28%] px-2 text-gray-600 text-center">{{plane.departure}}</p>
                         <p class="w-[28%] px-2 text-gray-600 text-center">{{plane.arrival}}</p>
                         <p class="w-[8%] px-2 text-blue-600 text-center">{{plane.alternative}}</p>
-                        <span @click="bookRoute()" class="cursor-pointer w-[8%] px-2 text-yellow-500 text-center">Prenota Ora</span>
+                        <span @click="bookRoute(plane.route_id, plane.fleet_id, plane.flight)" class="cursor-pointer w-[8%] px-2 text-yellow-500 text-center">Prenota Ora</span>
                     </li>
                 </ul>
                 <br>
@@ -34,6 +41,27 @@
         async mounted() {
             await this.$axios.get(`get_free_planes.php?id=82&route=${this.$route.params.flight}`)
                 .then((res) => this.planes = res.data)
+        },
+        methods:{
+            async bookRoute(route, plane, flightN){
+                this.$axios.get(`book.php?route=${route}&plane=${plane}&id=82`)
+                    .then(() => {
+                        this.$swal.fire({
+                            position: 'top-end',
+                            toast: true,
+                            icon: 'success',
+                            title: 'Volo # ' + flightN + 'prenotato',
+                            showConfirmButton: false,
+                            timer: 2000,
+                            footer: 'Royal Sky Service Booking System™'
+                        })
+                    })
+                        .then(() => {
+                            setTimeout(() => {
+                                this.$router.push('/')
+                            }, 2000)
+                        })
+            }
         }
     }
 </script>
